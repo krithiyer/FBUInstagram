269ee27659e31a4abab9ad417958fc85a0ccedcd
Krithika Iyer <ksiyer@fb.com>
2018-07-10 14:16:21 -0700

    Create Post
    User can create post and return to timeline page

--- app/src/main/java/me/krithiyer/fbuinstagram/HomeActivity.java
+++ app/src/main/java/me/krithiyer/fbuinstagram/HomeActivity.java
@@ -1,20 +1,14 @@
 package me.krithiyer.fbuinstagram;
 
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
-import android.support.v4.content.FileProvider;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.Toast;
 
 import com.parse.FindCallback;
 import com.parse.ParseException;
@@ -46,65 +40,84 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
 
-        descriptionInput = findViewById(R.id.tvDescription);
-        createButton = findViewById(R.id.btCreate);
-        refreshButton = findViewById(R.id.btRefresh);
-        logOutButton = findViewById(R.id.btLogOut);
-        cameraButton = findViewById(R.id.btCamera);
+        //descriptionInput = findViewById(R.id.tvDescription);
+        //createButton = findViewById(R.id.btCreate);
+        //refreshButton = findViewById(R.id.btRefresh);
+        //logOutButton = findViewById(R.id.btLogOut);
+        //cameraButton = findViewById(R.id.btCamera);
 
-        cameraButton.setOnClickListener(new View.OnClickListener() {
+        // setting navigation tasks
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
-            public void onClick(View view) {
-                onLaunchCamera(view);
-            }
-        });
-
-        logOutButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                ParseUser.logOut();
-                ParseUser currentUser = ParseUser.getCurrentUser();
-                if (currentUser == null) {
-                    Log.d("HomeActivity", "Logout successful!");
-                    final Intent i = new Intent(HomeActivity.this, MainActivity.class);
-                    startActivity(i);
-                    finish();
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()) {
+                    case R.id.create_button:
+                        Intent i = new Intent(HomeActivity.this, PostActivity.class);
+                        startActivity(i);
+                        return true;
 
                 }
+                return false;
             }
         });
 
-        createButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                final String description = descriptionInput.getText().toString();
-                final ParseUser user = ParseUser.getCurrentUser();
 
-                final File file = new File(imagePath);
-                final ParseFile parseFile = new ParseFile(file);
-                parseFile.saveInBackground(new SaveCallback() {
-                    @Override
-                    public void done(ParseException e) {
-                        if(e == null) {
-                            createPost(description, parseFile, user);
-                        } else {
-                            e.printStackTrace();
-                        }
+        // launching camera application
+        //cameraButton.setOnClickListener(new View.OnClickListener() {
+          //  @Override
+            //public void onClick(View view) {
+              //  onLaunchCamera(view);
+            //}
+       // });
 
-                    }
-                });
+        // logging out
+        //logOutButton.setOnClickListener(new View.OnClickListener() {
+          //  @Override
+            //public void onClick(View view) {
+              //  ParseUser.logOut();
+               // ParseUser currentUser = ParseUser.getCurrentUser();
+               // if (currentUser == null) {
+                 //   Log.d("HomeActivity", "Logout successful!");
+                   // final Intent i = new Intent(HomeActivity.this, MainActivity.class);
+                   // startActivity(i);
+                   // finish();
 
-            }
-        });
+               // }
+           // }
+       // });
+       // // creating a new post
+       // createButton.setOnClickListener(new View.OnClickListener() {
+         //   @Override
+          //  public void onClick(View view) {
+            //    final String description = descriptionInput.getText().toString();
+              //  final ParseUser user = ParseUser.getCurrentUser();
 
-        refreshButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                loadTopPosts();
-            }
-        });
+              //  final File file = new File(imagePath);
+              //  final ParseFile parseFile = new ParseFile(file);
+               // parseFile.saveInBackground(new SaveCallback() {
+                //    @Override
+                  //  public void done(ParseException e) {
+                    //    if(e == null) {
+                      //      createPost(description, parseFile, user);
+                       // } else {
+                         //   e.printStackTrace();
+                       // }
 
+                   // }
+               // });
+
+           // }
+       // });
+
+        // refreshing the timeline
+       // refreshButton.setOnClickListener(new View.OnClickListener() {
+         //   @Override
+           // public void onClick(View view) {
+             //   loadTopPosts();
+           // }
+       // });
 
     }
 
@@ -148,57 +161,6 @@ public class HomeActivity extends AppCompatActivity {
         });
     }
 
-    public void onLaunchCamera(View view) {
-        // create Intent to take a picture and return control to the calling application
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        // Create a File reference to access to future access
-        photoFile = getPhotoFileUri(photoFileName);
 
-        // wrap File object into a content provider
-        // required for API >= 24
-        // See https://guides.codepath.com/android/Sharing-Content-with-Intents#sharing-files-with-api-24-or-higher
-        Uri fileProvider = FileProvider.getUriForFile(HomeActivity.this, "com.codepath.fileprovider", photoFile);
-        intent.putExtra(MediaStore.EXTRA_OUTPUT, fileProvider);
 
-        // If you call startActivityForResult() using an intent that no app can handle, your app will crash.
-        // So as long as the result is not null, it's safe to use the intent.
-        if (intent.resolveActivity(getPackageManager()) != null) {
-            // Start the image capture intent to take photo
-            startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
-        }
-    }
-
-    // Returns the File for a photo stored on disk given the fileName
-    public File getPhotoFileUri(String fileName) {
-        // Get safe storage directory for photos
-        // Use `getExternalFilesDir` on Context to access package-specific directories.
-        // This way, we don't need to request external read/write runtime permissions.
-        File mediaStorageDir = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), APP_TAG);
-
-        // Create the storage directory if it does not exist
-        if (!mediaStorageDir.exists() && !mediaStorageDir.mkdirs()){
-            Log.d(APP_TAG, "failed to create directory");
-        }
-
-        // Return the file target for the photo based on filename
-        File file = new File(mediaStorageDir.getPath() + File.separator + fileName);
-
-        return file;
-    }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE) {
-            if (resultCode == RESULT_OK) {
-                // by this point we have the camera photo on disk
-                Bitmap takenImage = BitmapFactory.decodeFile(photoFile.getAbsolutePath());
-                // RESIZE BITMAP, see section below
-                // Load the taken image into a preview
-                ImageView ivCameraPhoto = (ImageView) findViewById(R.id.ivCameraPhoto);
-                ivCameraPhoto.setImageBitmap(takenImage);
-            } else { // Result was a failure
-                Toast.makeText(this, "Picture wasn't taken!", Toast.LENGTH_SHORT).show();
-            }
-        }
-    }
 }
